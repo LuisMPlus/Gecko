66570154fee0763f8c20a1b7e7e0f4f15b37a64e
LuisMPlus <lx147812345@gmail.com>
2025-06-29 19:24:37 -0300

    change: cambios en el header

--- src/components/closing/GoogleForm.jsx
+++ src/components/closing/GoogleForm.jsx
@@ -2,21 +2,18 @@ export function GoogleFormContainer() {
   return (
     <div
       className="group p-2 rounded-2xl shadow-sm border border-gray-100 transition-all duration-300 hover:shadow-lg"
-      style={{ backgroundColor: "#80b7c3" }}
     >
       <div className="bg-white rounded-xl overflow-hidden">
-        
-          <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLScOhSVzpFPqpiBSKrYShXXitMEudZE3hcZwrbUh0aTndMhfEg/viewform?embedded=true"
-            width="100%"
-            height="500"
-            /* frameborder="0"
-            marginheight="0"
-            marginwidth="0" */
-          >
-            Cargando…
-          </iframe>
-        
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScblQwfQMx5TwiGcfZ5X3svl9JRiIX-CkHu6LeOIxb-0WzPPQ/viewform?embedded=true"
+          width="100%"
+          height="600"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Cargando…
+        </iframe>
       </div>
     </div>
   );
