5d1befddd0d03e91f93fda0df115cb17f1630346
LuisMPlus <lx147812345@gmail.com>
2025-07-01 21:59:11 -0300

    feat: modificacion

--- src/Pages/Proposal.jsx
+++ src/Pages/Proposal.jsx
@@ -50,7 +50,7 @@ const PROTOTIPO_CONTENT = {
       description:
         "Argentina, como gran parte del mundo utiliza la notación FDI (La notación de la Federación Dental Internacional); para identificar los dientes.",
       detalles:
-        "El sistema utiliza dos números para definir cada diente: uno para el cuadrante y otro para el diente según su cuadrante. Todo esto permite al programa identificar fácilmente el historial dental, y así poder transformarlo en un archivo digital, cumple con la notación FDI.",
+        "Todo esto permite al programa identificar facilmente el historial dental, y asi poder transformarlo en un archivo digital, cumple con la notacion FDI.",
     },
   },
 };
@@ -92,6 +92,14 @@ export default function PrototipoPage() {
                 />
               </div>
             </div>
+
+            <div className="">
+              <img
+                src={explicacionprototipo}
+                alt="Explicación Prototipo"
+                className="w-full h-full object-contain mb-4 rounded-2xl"
+              />
+            </div>
           </div>
         </div>
       </section>
@@ -124,33 +132,24 @@ export default function PrototipoPage() {
         <section className="mb-20">
           <div className="bg-gradient-to-br from-blue-100 to-purple-100 rounded-3xl p-8 md:p-12">
             {/* Header de la funcionalidad */}
-            <div className="flex flex-col lg:flex-row items-center gap-8 mb-12">
-              <div className="lg:w-1/2">
-                <div className="text-center">
-                  <img
-                    src={explicacionprototipo}
-                    alt="Explicación Prototipo"
-                    className="w-full h-full object-contain mb-4 rounded-2xl"
-                  />
-                </div>
-              </div>
-
-              <div className="lg:w-1/2">
-                <div className="bg-white rounded-full w-16 h-16 flex items-center justify-center mb-4 ml-auto">
-                  <svg
-                    className="w-8 h-8 text-blue-900"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-                  </svg>
-                </div>
+            <div className="flex flex-row lg:flex-row items-start  mb-12">
+              <div>
                 <h2 className="text-3xl font-bold text-blue-900 mb-4">
                   {PROTOTIPO_CONTENT.escaneo.title}
                 </h2>
                 <p className="text-gray-700 text-lg">
                   {PROTOTIPO_CONTENT.escaneo.subtitle}
                 </p>
+              </div>
+
+              <div className="bg-white rounded-full w-16 h-16 flex items-center justify-center mb-4 ml-auto">
+                <svg
+                  className="w-8 h-8 text-blue-900"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                </svg>
               </div>
             </div>
 
